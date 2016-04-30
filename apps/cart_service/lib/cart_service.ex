c4e53a3cb199993aef34b686559c586800659d48
Andy Byczko <veriservus@gmail.com>
2016-04-30 14:05:29 +0100

    An agent wrapping

--- apps/cart_service/lib/cart_service.ex
+++ apps/cart_service/lib/cart_service.ex
@@ -1,7 +1,19 @@
 defmodule CartService do
 
+  def add_item(state, item) do
+    append_event(state, make_event(item, :add))
+  end
+
+  def remove_item(state, item) do
+    append_event(state, make_event(item, :remove))
+  end
+
   def append_event(state, event) do
     [ event | state ]
+  end
+
+  def make_event(item, action) do
+    %{item: item, action: action, ts: System.system_time}
   end
 
   def reduce([]) do
--- apps/cart_service/test/cart_service_test.exs
+++ apps/cart_service/test/cart_service_test.exs
@@ -5,4 +5,14 @@ defmodule CartServiceTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "adding and removing events" do
+    # Figure how to stub the time or fix this somehow
+    computed_state = CartService.add_item([], :food) |> CartService.add_item(:something_else) |> CartService.remove_item(:food)
+    expected_state = [%{action: :remove, item: :food, ts: 1462020085968145237},
+                      %{action: :add, item: :something_else, ts: 1462020085968142323},
+                      %{action: :add, item: :food, ts: 1462020085968137966}]
+
+    assert computed_state == expected_state
+  end
 end
