2bd50b6fda14765b6442be1ac172f0614cdd1181
Andy Byczko <veriservus@gmail.com>
2016-04-30 13:34:37 +0100

    Naive cart functions

--- apps/cart_service/lib/cart_service.ex
+++ apps/cart_service/lib/cart_service.ex
@@ -1,2 +1,15 @@
 defmodule CartService do
+
+  def append_event(state, event) do
+    [ event | state ]
+  end
+
+  def reduce([]) do
+    nil
+  end
+
+  def reduce([last_event | rest_of_events]) do
+    last_event
+  end
+
 end
