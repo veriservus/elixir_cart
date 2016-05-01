3df5079a12ddd67e1e51103e87488aee56ace42a
Andy Byczko <veriservus@gmail.com>
2016-05-01 10:38:58 +0100

    Naive view

--- apps/cart_api/web/views/cart_view.ex
+++ apps/cart_api/web/views/cart_view.ex
@@ -2,8 +2,8 @@ defmodule CartApi.CartView do
 
     use CartApi.Web, :view
 
-    def render(conn, %{user: user}) do
-      user
+    def render(conn, %{json_response: json_response}) do
+      json_response
     end
 
 end
