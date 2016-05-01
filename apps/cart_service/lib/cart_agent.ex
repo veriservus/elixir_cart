cbfb4efd612aa0ca6772a6403baf8cbfbd697baf
Andy Byczko <veriservus@gmail.com>
2016-05-01 10:39:23 +0100

    Allow starting of named carts

--- apps/cart_service/lib/cart_agent.ex
+++ apps/cart_service/lib/cart_agent.ex
@@ -1,16 +1,20 @@
-defmodule CartAgent do
-  alias CartService
+defmodule CartService.CartAgent do
+ #alias CartService
+
+ def start_link(user) do
+    Agent.start_link(fn -> [] end, name: user)
+ end
 
  def start_link do
    Agent.start_link(fn -> [] end)
  end
 
  def add_item(cart_agent, item) do
-   Agent.update(cart_agent, fn cart -> CartService.add_item(cart, item) end)
+   Agent.update(cart_agent, fn cart -> CartService.Service.add_item(cart, item) end)
  end
 
  def current_state(cart_agent) do
-   Agent.get(cart_agent, fn cart -> CartService.reduce(cart) end)
+   Agent.get(cart_agent, fn cart -> CartService.Service.reduce(cart) end)
  end
 
 end
