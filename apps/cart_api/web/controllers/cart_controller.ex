f363fc72edc347769b270935d16c952a60616683
Andy Byczko <veriservus@gmail.com>
2016-05-01 10:38:35 +0100

    Fleash out basic cart functions

--- apps/cart_api/web/controllers/cart_controller.ex
+++ apps/cart_api/web/controllers/cart_controller.ex
@@ -1,9 +1,26 @@
 defmodule CartApi.CartController do
 
     use CartApi.Web, :controller
+    require CartService.CartAgent
 
-    def index(conn, %{"user" => user}) do
-      render conn, user: user
+    def create(conn, %{"user" => user}) do
+      {:ok, pid} = CartService.CartAgent.start_link String.to_atom(user)
+      render conn, json_response: %{ created: user }
+    end
+
+    def add(conn, %{"user" => user}) do
+      CartService.CartAgent.add_item  String.to_atom(user), :food
+      render conn, json_response: %{ added: [user, :food] }
+    end
+
+    def remove(conn, %{"user" => user}) do
+      CartService.CartAgent.add_item  String.to_atom(user), :food
+      render conn, json_response: %{ removed: [user, :food] }
+    end
+
+    def show(conn, %{"user" => user}) do
+      state = CartService.CartAgent.current_state String.to_atom(user)
+      render conn, json_response: state
     end
 
 end
